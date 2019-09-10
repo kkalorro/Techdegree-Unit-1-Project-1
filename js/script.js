5d4ee29d379b11fe153a1e384a11271993dc4bb7
keith-kit <keithkit@users.noreply.github.com>
2019-09-10 16:42:44 -0700

    New Quote Properties.  Cleaned comments.  Semi-colon corrections.
    Converted Quotes.Tags from String to Array.  Added Quotes.ID as a Unique Identifier with future application mostly for end-user.  Cleaned up comments to be only before code.  Added missing semi-colons.

--- js/script.js
+++ js/script.js
@@ -6,87 +6,93 @@ Possible future improvements:
   2) Ensure random background color is not the current background color.
 */
 
-//GLOBAL DECLARATIONS
+//GLOBAL DECLARATIONS.
 
 var quotes = [
-  //Complete Quote 1
+  //Complete Quote 1.
   {
+    //id is a unique identifier for the quote object.
+    id: 1,
     quote: 'For you, the day Bison graced your village was the most important day of your life. But for me, it was Tuesday.',
     source: 'Bison',
     citation: 'Street Fighter (movie)',
     year: 1994,
-    tags: 'movie'
+    tags: ['movie']
   },
-  //Complete Quote 2
+  //Complete Quote 2.
   {
-    quote: 'Hasta la vista, baby.',
-    source: 'The Terminator',
-    citation: 'Terminator 2: Judgement Day',
-    year: 1991,
-    tags: 'movie'
-  },
-  //Quote without a Year 1
-  {
-    quote: 'You must construct additional Pylons',
+    id: 2,
+    quote: 'You must construct additional Pylons.',
     source: 'Protoss VO',
     citation: 'Starcraft',
- //   year: 1998,
-    tags: 'game'
+    year: 1998,
+    tags: ['game']
   },
-  //Quote without a Year 2
+  //Quote without a Year.
   {
+    id: 3,
     quote: 'That boy ain\'t right.',
     source: 'Hank Hill',
     citation: 'King of the Hill',
- //   year: 1997,
-    tags: 'cartoon'
+    tags: ['cartoon']
   },
-  //Quote without a Citation or a Year
+  //Quote without a Year and multiple tags.
   {
+    id: 4,
+    quote: 'Cowabunga!',
+    source: 'Teenage Mutant Ninja Turtles',
+    citation: 'Various',
+    tags: ['cartoon', 'movie']
+  },
+  //Quote without a Citation or a Year.
+  {
+    id: 5,
     quote: 'Show me your moves!',
     source: 'Captain Falcon',
-//    citation: 'Super Smash Bros.',
-//    year: 1999,
-    tags: 'game'
+    tags: ['game']
   },
 ];
 console.log(quotes);
 
-//FUNCTIONS
+//FUNCTIONS.
 
-//Return random number
+//Return random number.
 function getRandomNumber(ceil) {
   return Math.floor(Math.random() * ceil);
 }
 
-//Return random quote from quotes array
+//Return random quote from the entire quotes array.
 function getRandomQuote () {
   var randNumber = getRandomNumber(quotes.length);
   return quotes[randNumber];
 }
 
-//Display selected quote and selected background color
+//Display selected quote and selected background color.
 function printQuote() {
-  console.log(Date.now());  //This is to see if this function is being called consistently.
-  //Declarations
+  //This is to check if this function is being called at a consistent interval.
+  console.log(Date.now());
+  //Declarations.
   var writeHTML = '';
   var newQuote = getRandomQuote();
+  var id = newQuote.id;
   var quote = newQuote.quote;
   var source = newQuote.source;
   var citation = newQuote.citation;
   var year = newQuote.year;
   var tags = newQuote.tags;
+  //Selected quote details.
+  console.log('Quote ID: ' + id + '.  Number of Quote Tags: ' + tags.length + '.  Quote Text: "' + quote + '"');
 
-  //Write HTML for caption based on tags
-  if (tags === 'game') {
-    writeHTML += '<div id="game"><center>Quotes for gamers</center></div><br>'
-  } else if (tags === 'movie') {
-    writeHTML += '<div id="movie"><center>Quotes for cinema buffs</center></div><br>'
-  } else if (tags === 'cartoon') {
-    writeHTML += '<div id="cartoon"><center>Quotes for those who like animation</center></div><br>'
+    //Write HTML for caption based on tags.
+    if (tags.length === 1) {
+    writeHTML += '<div id="tags"><center><u>' + tags[0].toUpperCase() + '</u></center></div><br>';
+  } else if (tags.length > 1) {
+    writeHTML += '<div id="tags"><center><u>' + tags.join(', ').toUpperCase() + '</u></center></div><br>';
+  } else {
+    console.log('Quote has no tags.');
   }
 
-  //Write HTML for quote
+  //Write HTML for quote.
   writeHTML += '<p class="quote">' + quote + '</p><p class="source">' + source;
   if (citation) {
     writeHTML += '<span class="citation">' + citation + '</span>';
@@ -100,24 +106,30 @@ function printQuote() {
   }
   writeHTML += '</p>';
 
-  //Print HTML to page
-  randomColor();  //Change background color.
+  //Change background color.
+  randomColor();
   var outputDiv = document.getElementById('quote-box');
-  console.log(writeHTML);  //To console: HTML contents.
+  //Log HTML code on console for debugging.
+  console.log(writeHTML);
+  //Print HTML to page.
   outputDiv.innerHTML = writeHTML;
 }
   
-//Produce random color
+//Produce random color.
 function randomColor() {
-  var randomLevel = 'rgb(';  //Create the starting HTML tag for RGB color.
-  randomLevel += getRandomNumber(256) + ',';  //Produces a random intensity of Red.
-  randomLevel += getRandomNumber(256) + ',';  //Produces a random intensity of Green.
-  randomLevel += getRandomNumber(256) + ')';  //Produces a random intensity of Blue.
+  //Create the HTML tag for RGB color.
+  var randomLevel = 'rgb(';
+  randomLevel += getRandomNumber(256) + ',';
+  randomLevel += getRandomNumber(256) + ',';
+  randomLevel += getRandomNumber(256) + ')';
   document.body.style.backgroundColor = randomLevel;
 }
 
 //EXECUTION
 
-printQuote();  //Start with a random quote.
-setInterval(printQuote, 20000);  //Call function on a 20sec interval.
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);  //Call button functionality.
+//Start with a random quote.
+printQuote();
+//Call random quote function on a 20sec interval.
+setInterval(printQuote, 20000);
+//Preexisting button functionality.
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
